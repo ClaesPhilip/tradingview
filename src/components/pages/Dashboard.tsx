999fdf5e8e48f51447671c4d383a50738406f5a8
Philip Löfgren <55080120+ClaesPhilip@users.noreply.github.com>
2021-01-05 15:39:32 +0100

    API

--- src/components/pages/Dashboard.tsx
+++ src/components/pages/Dashboard.tsx
@@ -5,45 +5,136 @@ import Message from '../UI/Message';
 import { setSuccess } from '../../store/actions/authActions';
 import { RootState } from '../../store';
 import { Link } from 'react-router-dom';
-import axios from 'axios';
 
-const options: any = {
-  method: 'GET',
-  url: 'https://alpha-vantage.p.rapidapi.com/query',
-  params: {function: 'GLOBAL_QUOTE', symbol: 'TSLA'},
-  headers: {
-    'x-rapidapi-key': '3ac8d4ae88msh5e51fe37d8246cbp1d1d55jsnaf860dc19453',
-    'x-rapidapi-host': 'alpha-vantage.p.rapidapi.com'
-  }
-};
+import axios from "axios";
 
-axios.request(options).then(function (response) {
-	console.log(response.data);
-}).catch(function (error) {
-	console.error(error);
-});
-
-const Dashboard: FC = () => {
-  const { user, needVerification, success } = useSelector((state: RootState) => state.auth);
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    if(success) {
-      dispatch(setSuccess(''));
-    }
-  }, [success, dispatch]);
-
-
-  return(
-    <section className="section">
-      <div className="container">
-        {needVerification && <Message type="success" msg="Please verify your email address." />}
-        <h1 className="is-size-1">Welcome {user?.firstName}</h1>
-        <Link to='/test'>TEST ME</Link>
-        
-      </div>
-    </section>
-  );
+interface ICrypto {
+  userId: number;
+  id: string;
+  name: string;
+  market_cap: number;
+  current_price: number;
+  image: string;
+  total_volume: number;
+  symbol: string;
+  price_change_percentage_24h: number;
 }
 
-export default Dashboard;
+const defaultProps:ICrypto[] = [];
+
+const CryptoCurrency: React.FC = () => {
+  const [crypto, setCryptos]: [ICrypto[], (posts: ICrypto[]) => void] = React.useState(defaultProps);
+  const [loading, setLoading]: [boolean, (loading: boolean) => void] = React.useState<boolean>(true);
+  const [error, setError]: [string, (error: string) => void] = React.useState("");
+  const [search, setSearch]: [string, (error: string) => void] = React.useState("");
+
+  React.useEffect(() => {
+    axios
+      .get<ICrypto[]>("https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&order=market_cap_desc&per_page=100&page=1&sparkline=false")
+      .then(response => {
+        setCryptos(response.data);
+        setLoading(false);
+      }).catch(ex => {
+        const error =
+        ex.response.status === 404
+          ? "Resource not found"
+          : "An unexpected error has occurred";
+        setError(error);
+        setLoading(false);
+      });
+  }, []);
+
+  const handleChange = (e: { target: { value: string; }; }) => {
+    setSearch(e.target.value);
+  };
+
+  const filteredCoins = crypto.filter(crypto =>
+    crypto.name.toLowerCase().includes(search.toLowerCase())
+  );
+
+  return (
+    <div className="App">
+  <ul className="posts">
+    <input type="text" onChange={handleChange}></input>
+    {crypto.map((crypto) => (
+     <li key={crypto.id}>
+      <h3>{crypto.id}</h3>
+      <p>{crypto.current_price}</p>
+     </li>
+   ))}
+  </ul>
+  {error && <p className="error">{error}</p>}
+</div>
+  )
+}
+
+export default CryptoCurrency;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const options: any = {
+//   method: 'GET',
+//   url: 'https://coingecko.p.rapidapi.com/coins/markets',
+//   params: {vs_currency: 'usd', page: '1', per_page: '100', order: 'market_cap_desc'},
+//   headers: {
+//     'x-rapidapi-key': '3ac8d4ae88msh5e51fe37d8246cbp1d1d55jsnaf860dc19453',
+//     'x-rapidapi-host': 'coingecko.p.rapidapi.com'
+//   }
+// };
+
+// axios.request(options).then(function (response) {
+// 	console.log(response.data);
+// }).catch(function (error) {
+// 	console.error(error);
+// });
+
+// const Dashboard: FC = () => {
+//   const { user, needVerification, success } = useSelector((state: RootState) => state.auth);
+//   const dispatch = useDispatch();
+
+//   useEffect(() => {
+//     if(success) {
+//       dispatch(setSuccess(''));
+//     }
+//   }, [success, dispatch]);
+
+
+//   return(
+//     <section className="section">
+//       <div className="container">
+//         {needVerification && <Message type="success" msg="Please verify your email address." />}
+//         <h1 className="is-size-1">Welcome {user?.firstName}</h1>
+//         <Link to='/test'>TEST ME</Link>
+        
+//       </div>
+//     </section>
+//   );
+// }
+
+// export default Dashboard;
