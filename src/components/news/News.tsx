f9d221b628249cad7d0c8163dab22174b5e92cdb
Philip Löfgren <55080120+ClaesPhilip@users.noreply.github.com>
2021-01-12 00:05:26 +0100

    news test

--- src/components/news/News.tsx
+++ src/components/news/News.tsx
@@ -1,10 +1,9 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import './News.css';
 
-
 export interface INews {
-    [x: string]: any;
+    articles: any;
     id: string;
     author: string;
     content: string;
@@ -26,23 +25,32 @@ const News: React.FC = () => {
 
   React.useEffect(() => {
     const url = "http://newsapi.org/v2/everything?q=bitcoin&from=2020-12-11&sortBy=publishedAt&apiKey=2465fb66cd5b42d78cc87f952a865b78";
-    const req = new Request(url);
-    fetch(req)
+    const value = new Request(url);
+    fetch(value)
     .then(function(response) {
         console.log(response.json());
-    })
-  }, []);
+        })
 
+
+        Promise.resolve('Success').then(function(value) {
+          console.log(value);
+        }, function(value) {
+          return value;
+        });
+
+        
+  }, []);
+  
   return (
   <div>
           {news.map((news) => {
               return (
                 <div key={news.id}>  
-                <div>{news.articles.lenght}</div>
+                <div>{`${console.log(news.articles.title)}`}</div>
                 </div>
           )})}
         {error && <p className="error">{error}</p>}
-  </div>
+  </div>  
   )
 }
 
