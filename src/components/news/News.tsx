83ee693afcfe1f4526c0e4b8c2cf0037ccc607fe
Philip Löfgren <55080120+ClaesPhilip@users.noreply.github.com>
2021-01-24 16:55:11 +0100

    Update News.tsx

--- src/components/news/News.tsx
+++ src/components/news/News.tsx
@@ -1,57 +1,137 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
 
-import './News.scss';
+// import './styles/News.scss';
 
-export interface INews {
-    articles: any;
-    id: string;
-    author: string;
-    content: string;
-    description: string;
-    publishedAt: number | string;
-    source: string;
-    name: string;
-    title: string;
-    url: string;
-    urlToImage: string;
-}
+// interface INews {
+//     id: string;
+//     author: string;
+//     content: string;
+//     description: string;
+//     publishedAt: number | string;
+//     source: string;
+//     name: string;
+//     title: string;
+//     url: string;
+//     urlToImage: string;
+// }
 
-const defaultProps:INews[] = [];
+// const defaultProps:INews[] = [];
 
-const News: React.FC = () => {
-  const [news, setNews]: [INews[], (posts: INews[]) => void] = React.useState(defaultProps);
-  const [error, setError]: [string, (error: string) => void] = React.useState("");
-  const [search, setSearch]: [string, (search: string) => void] = React.useState("");
+// const News: React.FC = () => {
+//   const [news, setNews]: [INews[], (posts: INews[]) => void] = React.useState(defaultProps);
+//   const [error, setError]: [string, (error: string) => void] = React.useState("");
+//   const [search, setSearch]: [string, (search: string) => void] = React.useState("");
 
-  React.useEffect(() => {
-    const url = "http://newsapi.org/v2/everything?q=bitcoin&from=2020-12-11&sortBy=publishedAt&apiKey=2465fb66cd5b42d78cc87f952a865b78";
-    const value = new Request(url);
-    fetch(value)
-    .then(function(response) {
-        console.log(response.json());
-        })
+//   React.useEffect(() => {
+//     const News = "http://newsapi.org/v2/everything?q=bitcoin&from=2021-01-19&sortBy=publishedAt&apiKey=2465fb66cd5b42d78cc87f952a865b78";
+//     const value = new Request(News);
+//     fetch(value)
+//     .then(function(response) {
+//         console.log(response.json());
+//         })
 
 
-        Promise.resolve('Success').then(function(value) {
-          console.log(value);
-        }, function(value) {
-          return value;
-        });
+//         Promise.resolve('Success').then(function(value) {
+//           console.log(value);
+//         }, function(value) {
+//           return value;
+//         });
 
         
-  }, []);
+//   }, []);
   
-  return (
-  <div>
-          {news.map((news) => {
-              return (
-                <div key={news.id}>  
-                <div>{`${console.log(news.articles.title)}`}</div>
-                </div>
-          )})}
-        {error && <p className="error">{error}</p>}
-  </div>  
-  )
+//   return (
+//   <div>
+//           {news.map((News) => {
+//               return (
+//                 <div key={News.id}>  
+//                 <div>{News.title}</div>
+//                 </div>
+//           )})}
+//         {error && <p className="error">{error}</p>}
+//   </div>  
+//   )
+// }
+
+// export default News;
+
+
+
+
+
+
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
+import NewsItem from './NewsItem';
+import axios from 'axios';
+import { articleProps } from './types';
+
+const NewsListBlock = styled.div`
+	padding: 0.2rem 1rem 1rem 1rem;
+	background-color: rgb(245, 245, 245);
+	color: #3c3c3c;
+	box-sizing: border-box;
+	header > * {
+		margin: 0.5rem;
+	}
+`;
+
+type NewsListProps = {
+	category: string;
+};
+
+function NewsList({ category }: NewsListProps) {
+	const [datetime] = useState(new Date());
+	const [articles, setArticles] = useState([]);
+	const [loading, setLoading] = useState(false);
+
+	useEffect(() => {
+		const fetchData = async () => {
+			setLoading(true);
+			try {
+				// const query = category === 'all' ? '' : `&category=${category}`;
+				const response = await axios.get(
+					`http://newsapi.org/v2/everything?q=bitcoin&language=en&from=${datetime}&sortBy=publishedAt&apiKey=2465fb66cd5b42d78cc87f952a865b78`,
+				);
+        console.log(response);
+				setArticles(response.data.articles);
+			} catch (e) {
+				console.log(e);
+			}
+			setLoading(false);
+		};
+		fetchData();
+	}, []);
+
+	if (loading) {
+		return <NewsListBlock>Loading...</NewsListBlock>;
+	}
+	if (!articles) return null;
+
+	return (
+		<NewsListBlock>
+			<header>
+				<h2>News</h2>
+				<p>
+					{datetime.toLocaleDateString() +
+						'  ' +
+						datetime.toLocaleTimeString()}
+				</p>
+			</header>
+			{articles.map((article: articleProps) => (
+				<NewsItem
+					key={article.url}
+					title={article.title}
+					description={article.description}
+					url={article.url}
+					urlToImage={article.urlToImage}
+          author={article.author}
+				/>
+			))}
+		</NewsListBlock>
+	);
 }
 
-export default News;
+export default NewsList;
+
+
